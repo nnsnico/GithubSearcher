15547bf3a89d1c4befe3bc6584ab274e9ac9d00d
Unknown <yuji.jampan.blue@gmail.com>
2017-08-16 11:22:20 +0900

    DBクラスとテーブルクラスの作成

---
+++ GithubSearcher/app/src/main/java/com/github/intern/yuji/githubsearcher/db/InternalGithubRepo.java
@@ -0,0 +1,33 @@
+package com.github.intern.yuji.githubsearcher.db;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+/**
+ * Created by nns on 2017/08/16.
+ */
+@Table(database = InternalGithubDB.class)
+public class InternalGithubRepo extends BaseModel {
+    @PrimaryKey(autoincrement = true)
+    private int id;
+    @Column
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
